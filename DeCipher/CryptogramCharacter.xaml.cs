b2dbfb02cd2d3c4685576743ddcae5ff809f9008
jaredkrinke <jared.krinke@gmail.com>
2013-11-12 17:06:35 -0800

    Added focus, background color

--- DeCipher/CryptogramCharacter.xaml.cs
+++ DeCipher/CryptogramCharacter.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -21,6 +23,29 @@ namespace DeCipher
         public CryptogramCharacter()
         {
             this.InitializeComponent();
+        }
+
+        private void UserControl_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            this.Focus(Windows.UI.Xaml.FocusState.Programmatic);
+        }
+
+        private void UserControl_GotFocus(object sender, RoutedEventArgs e)
+        {
+            // TODO: Use a style or something
+            this.grid.Background = new SolidColorBrush(Color.FromArgb(255, 255, 0, 0));
+        }
+
+        private void UserControl_LostFocus(object sender, RoutedEventArgs e)
+        {
+            // TODO: Use a style or something
+            this.grid.Background = new SolidColorBrush(Color.FromArgb(0, 0, 0, 0));
+        }
+
+        private void UserControl_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            // TODO: I don't think this is getting called...
+            Debug.Assert(false);
         }
     }
 }
